0830c425c8d3ea77f7de37195e8532893101f578
Riccardo Carlesso <palladiusbonton+git@gmail.com>
2013-02-17 10:31:44 +0100

    funge

--- lib/palladius.rb
+++ lib/palladius.rb
@@ -1,5 +1,7 @@
 
-viola 'This is palladius Gem'
+def print_purple(s)
+  print "\033[1;35m#{s}\033[0m\n"
+end
 
 # TODO get version programmatically, I guess it will be sth like ECHOE.VERSION
 # because it's echoe to give it to me. If too difficult, roll back to version()
@@ -10,3 +12,6 @@ end
 def palladius_qwerty
 	"palladius library/gem/plugin v#{palladius_version}. Please be patient, more to come!"
 end
+
+# TODO removeme
+print_purple 'This is palladius Gem. See http://www.palladius.it/'
