6ed9ca2107d1dfbbcf3c01bf2726ee8e4cfceb2c
Riccardo Carlesso <palladiusbonton+git@gmail.com>
2013-02-17 10:20:18 +0100

    005

--- lib/palladius.rb
+++ lib/palladius.rb
@@ -1,3 +1,12 @@
 
 viola 'This is palladius Gem'
 
+# TODO get version programmatically, I guess it will be sth like ECHOE.VERSION
+# because it's echoe to give it to me. If too difficult, roll back to version()
+def palladius_version
+	:TODO
+end
+
+def palladius_qwerty
+	"palladius library/gem/plugin v#{palladius_version}. Please be patient, more to come!"
+end
